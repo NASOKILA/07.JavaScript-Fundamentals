811c6b8cf6c71f2dc6ec33f1f6facb8baa34a85c
Atanas Kambitov <atanasskambitov@gmail.com>
2018-06-20 11:09:43 +0300

    Strings And Regex Exercises

---
+++ 09.Strings And Regex Exercises/01.Split a String With A Delimeter.js
@@ -0,0 +1,40 @@
+
+function solve(text, delimeter) {
+    let result = text.split(delimeter);
+    result.forEach(e => console.log(e));
+}
+
+//solve('One-Two-Three-Four-Five', '-');
+
+
+function s(text, delimeter){
+    text.split(delimeter).forEach(e => console.log(e));
+}
+s('One-Two-Three-Four-Five', '-');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 09.Strings And Regex Exercises/02. Repeat a String N Times.js
@@ -0,0 +1,27 @@
+
+function solve(str, times) {
+
+    let result = '';
+    for(let i = 1; i <= times; i++)
+    {
+        result += str;
+    }
+
+    console.log(result);
+}
+
+//solve('repeat', 5);
+
+
+
+function s(str, n) {
+    console.log(str.repeat(n));
+}
+
+s('repeat', 5);
+
+
+
+
+
+
