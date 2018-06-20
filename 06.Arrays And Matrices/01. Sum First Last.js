ef5245ad9f1fd36b006f9f691871b697212cf90e
Atanas Kambitov <atanasskambitov@gmail.com>
2018-06-20 11:03:23 +0300

    Arrays And Matrices

---
+++ 06.Arrays And Matrices/01. Sum First Last.js
@@ -0,0 +1,25 @@
+
+function solve(args) {
+
+    let sum = Number(args[0]) +
+              Number(args[args.length - 1]);
+    return sum;
+}
+
+solve(['20','30','40']);
+console.log(solve([20, 30, 40]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
