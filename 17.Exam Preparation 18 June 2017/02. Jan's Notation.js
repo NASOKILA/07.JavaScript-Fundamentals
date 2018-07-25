d1cbff367201d2bd31c9f0dfc02bf65915f1ac52
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 03:43:55 +0300

    Exam Preparation 18 June 2017

--- 17.Exam Preparation 18 June 2017/02. Jan's Notation.js
+++ 17.Exam Preparation 18 June 2017/02. Jan's Notation.js
@@ -1,7 +1,6 @@
 
 function solve(args) {
 
-
     let numbers = [];
     let symbols = [];
 
@@ -11,7 +10,6 @@ function solve(args) {
             || currentElement === '*'
             || currentElement === '+'
             || currentElement === '-') {
-            //its a symbol
             symbols.push(currentElement);
         }
     }
@@ -25,7 +23,6 @@ function solve(args) {
             || currentElement === '*'
             || currentElement === '+'
             || currentElement === '-') {
-            //its a symbol
 
             if (numbers.length <= 1) {
                 console.log('Error: not enough operands!');
@@ -34,33 +31,26 @@ function solve(args) {
 
             let secondNum = numbers.pop();
             let firstNum = numbers.pop();
-
             let result = findResult(firstNum, secondNum, currentElement);
+
             numbers.push(result);
-            
             symbols.pop();
-            
-            //The end comes when there are no more symbols and only one number
+
             if (numbers.length === 1 && symbols.length == 0)
                 break;
-
         }
         else {
-            //its a number
             numbers.push(currentElement);
-
         }
-
     }
 
-    //ako imame poveche ot edno chislo v masiva znachi ni lipsvat znaci
     if (numbers.length > 1)
         console.log('Error: too many operands!');
     else
         console.log(numbers[0]);
 
-
     function findResult(firstNum, secondNum, operand) {
+
         let result = 0;
 
         if (operand === "*")
@@ -74,5 +64,4 @@ function solve(args) {
 
         return result;
     }
-
 }
