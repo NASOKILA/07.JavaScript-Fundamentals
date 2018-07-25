ea6114be83bc1897e61dd0a5e996b1379ed83865
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 03:44:44 +0300

    Exam Preparation 18 June 2017

--- 17.Exam Preparation 18 June 2017/04. Galactic Elections.js
+++ 17.Exam Preparation 18 June 2017/04. Galactic Elections.js
@@ -7,7 +7,7 @@ function solve(ballots) {
     for (let ball of ballots) {
 
         totalVotesAtAll += ball.votes;
-        
+
         if (starSystems.has(ball.system)) {
 
             let currentCancidateAndListOfVotes = starSystems.get(ball.system);
@@ -32,31 +32,30 @@ function solve(ballots) {
     let sistemsWinners = [];
 
     for (let system of starSystems) {
-        
+
         let systemWinner = [...[...system][1]][0];
         systemWinner.push(system[0]);
-    
+
         for (let candidate of [...system][1]) {
             let winnerVotes = Number(systemWinner[1]);
             let challengerVotes = Number(candidate[1]);
 
-            if(winnerVotes < challengerVotes)
+            if (winnerVotes < challengerVotes)
                 systemWinner = candidate;
         }
 
         for (let candidate of [...system][1]) {
-            
-            if(candidate[0] !== systemWinner[0])
+
+            if (candidate[0] !== systemWinner[0])
                 systemWinner[1] += Number(candidate[1]);
         }
 
         sistemsWinners.push(systemWinner);
     }
 
-    let winnerOfWinners = sistemsWinners.sort((a,b) => a[1] <= b[1])[0];
+    let winnerOfWinners = sistemsWinners.sort((a, b) => a[1] <= b[1])[0];
 
-    if(sistemsWinners.every(e => e[0] === winnerOfWinners[0]))
-    {
+    if (sistemsWinners.every(e => e[0] === winnerOfWinners[0])) {
         let totalSumOfVotes = 0;
         sistemsWinners.map(w => totalSumOfVotes += w[1]);
 
@@ -67,49 +66,48 @@ function solve(ballots) {
 
     let totalSumOfVotes = 0;
     sistemsWinners.map(w => totalSumOfVotes += w[1]);
-    
-    if(winnerOfWinners[1] > (totalSumOfVotes / 2))
-    {
+
+    if (winnerOfWinners[1] > (totalSumOfVotes / 2)) {
         console.log(winnerOfWinners[0] + ' wins with ' + winnerOfWinners[1] + ' votes');
-        
+
         sistemsWinners.shift();
         let runnerUpWinner = sistemsWinners.shift();
 
         sistemsWinners.filter(w => w[0] === runnerUpWinner[0])
-        .map(o => runnerUpWinner[1] += o[1]);
+            .map(o => runnerUpWinner[1] += o[1]);
 
         console.log('Runner up: ' + runnerUpWinner[0]);
-    
+
         let runnerUpsystems = [];
-    
-        for (let system of [...starSystems]) {        
+
+        for (let system of [...starSystems]) {
             for (let candidate of [...system][1]) {
-                if(candidate[0] === runnerUpWinner[0]
-                && winnerOfWinners[2] !== system[0]){
+                if (candidate[0] === runnerUpWinner[0]
+                    && winnerOfWinners[2] !== system[0]) {
 
                     let runnerUpTotalSum = 0;
                     [...[...system][1]].map(w => runnerUpTotalSum += w[1]);
-                    runnerUpsystems.push({'name':system[0], 'votes':runnerUpTotalSum});
+                    runnerUpsystems.push({ 'name': system[0], 'votes': runnerUpTotalSum });
                     break;
-                }    
+                }
             }
         }
 
-        for(let sys of runnerUpsystems.sort((a,b) => (a.votes <= b.votes))){
+        for (let sys of runnerUpsystems.sort((a, b) => (a.votes <= b.votes))) {
             console.log(sys.name + ': ' + sys.votes);
         }
 
     }
     else {
-        
+
         sistemsWinners.shift();
         sistemsWinners.filter(w => w[0] === winnerOfWinners[0])
-        .map(o => winnerOfWinners[1] += o[1]);
+            .map(o => winnerOfWinners[1] += o[1]);
 
         let runnerUpWinner = sistemsWinners.shift();
         sistemsWinners.shift();
         sistemsWinners.filter(w => w[0] === runnerUpWinner[0])
-        .map(o => runnerUpWinner[1] += o[1]);
+            .map(o => runnerUpWinner[1] += o[1]);
 
         let winnerPercentage = Math.floor(100 / (totalVotesAtAll / winnerOfWinners[1]));
         let runnerPercentage = Math.floor(100 / (totalVotesAtAll / runnerUpWinner[1]));
@@ -118,10 +116,10 @@ function solve(ballots) {
 }
 
 solve(
-    [ { system: 'Tau',     candidate: 'Flying Shrimp', votes: 150 },
-      { system: 'Tau',     candidate: 'Space Cow',     votes: 100 },
-      { system: 'Theta',   candidate: 'Space Cow',     votes: 10 },
-      { system: 'Sigma',   candidate: 'Space Cow',     votes: 200 },
-      { system: 'Sigma',   candidate: 'Flying Shrimp', votes: 75 },
-      { system: 'Omicron', candidate: 'Flying Shrimp', votes: 50 },
-      { system: 'Omicron', candidate: 'Octocat',       votes: 75 } ]);
+    [{ system: 'Tau', candidate: 'Flying Shrimp', votes: 150 },
+    { system: 'Tau', candidate: 'Space Cow', votes: 100 },
+    { system: 'Theta', candidate: 'Space Cow', votes: 10 },
+    { system: 'Sigma', candidate: 'Space Cow', votes: 200 },
+    { system: 'Sigma', candidate: 'Flying Shrimp', votes: 75 },
+    { system: 'Omicron', candidate: 'Flying Shrimp', votes: 50 },
+    { system: 'Omicron', candidate: 'Octocat', votes: 75 }]);
