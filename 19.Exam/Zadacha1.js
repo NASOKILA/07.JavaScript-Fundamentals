155fc89b59181cc1b7f93a9ee001667c8f0cb4da
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 03:55:49 +0300

    Exam

--- 19.Exam/Zadacha1.js
+++ 19.Exam/Zadacha1.js
@@ -1,39 +1,29 @@
 
-
-
-
 function solve(args) {
 
     let boughtBitcoins = 0;
     let totalMoney = 0;
     let firstDayBoughtBitcoin = 0;
     let firstDayFound = false;
-
     let count = 1;
-    for(let i = 0; i < args.length; i++)
-    {
+
+    for(let i = 0; i < args.length; i++){
+
         let goldDiggedInGrams = Number(args[i]);
 
-        //stealing every third day
-        if(count % 3 === 0 && count > 0)
-        {
-            //30%
-            let thirtyPercent = goldDiggedInGrams - (goldDiggedInGrams / 2) 
-            - (goldDiggedInGrams / 5);  
-
-            goldDiggedInGrams -= thirtyPercent;            
+        if(count % 3 === 0 && count > 0){
             
+            let thirtyPercent = goldDiggedInGrams - (goldDiggedInGrams / 2) - (goldDiggedInGrams / 5);  
+            goldDiggedInGrams -= thirtyPercent;            
         }
+
         let leva = 67.51 * goldDiggedInGrams;
         totalMoney += Number(leva);
         
-        
-            
-        
-        while(totalMoney >= 11949.16)
-        {
-            if(!firstDayFound)
-            {
+        while(totalMoney >= 11949.16){
+
+            if(!firstDayFound){
+
                 firstDayBoughtBitcoin = count;
                 firstDayFound = true;
             }
@@ -42,10 +32,6 @@ function solve(args) {
             totalMoney -= 11949.16;
         }
 
-            //Vzimame purviq den v koito sme kupili bitcoin:
-           
-            //totalMoney = Math.round(totalMoney).toFixed(2);
-        
         count++;
     }
 
@@ -53,30 +39,16 @@ function solve(args) {
     
     if(boughtBitcoins > 0)
         console.log("Day of the first purchased bitcoin: " + firstDayBoughtBitcoin);
-    
-    
+        
         console.log("Left money: " + totalMoney.toFixed(2) +" lv.");
-
-
 }
 
-
 solve(['3124.15', '504.212', '2511.124']);
+
 console.log();
+
 solve(['50', '100']);
+
 console.log();
+
 solve(['100', '200', '300']);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 19.Exam/Zadacha2.js
+++ 19.Exam/Zadacha2.js
@@ -1,12 +1,9 @@
 
-
-
 function solve(map, input) {
 
     let mapOfSofia = [];
 
     for (let i = 0; i < map.length; i++) {
-
         let row = map[i].split(' ').map(e => Number(e));
         mapOfSofia.push(row);
     }
@@ -14,35 +11,27 @@ function solve(map, input) {
     for (let item of input) {
 
         let tokens = item.split(' ');
-
         let command = tokens[0];
         let index = Number(tokens[1]);
 
         if (command === 'breeze') {
 
             for (let i = 0; i < mapOfSofia.length; i++) {
-
-                //polluted blicks cannot go below 0
                 mapOfSofia[index][i] -= 15;
 
                 if (mapOfSofia[index][i] < 0)
                     mapOfSofia[index][i] = 0;
-
             }
         }
         else if (command === 'gale') {
 
             for (let i = 0; i < mapOfSofia.length; i++) {
 
-                //polluted blicks cannot go below 0
-                
                 mapOfSofia[i][index] -= 20;
-            
-               // if (mapOfSofia[index][i] - 20 >= 0)
-                    if(mapOfSofia[i][index] < 0)
-                        mapOfSofia[i][index] = 0;
-                
-                    }
+
+                if (mapOfSofia[i][index] < 0)
+                    mapOfSofia[i][index] = 0;
+            }
 
         }
         else if (command === 'smog') {
@@ -52,38 +41,26 @@ function solve(map, input) {
                     mapOfSofia[i][j] += index;
                 }
             }
-
         }
-
     }
-
-
-    //find pollute areas:
 
     let polluteBlocks = [];
 
     for (let i = 0; i < mapOfSofia.length; i++) {
         for (let j = 0; j < mapOfSofia.length; j++) {
-            
             let currentBlock = mapOfSofia[i][j];
 
-            if(currentBlock >= 50){
-                polluteBlocks.push('[' + i + '-' + j +']');
+            if (currentBlock >= 50) {
+                polluteBlocks.push('[' + i + '-' + j + ']');
             }
-
         }
     }
 
-
-    //print pollute areas:
-    if(polluteBlocks.length > 0)
+    if (polluteBlocks.length > 0)
         console.log('Polluted areas: ' + polluteBlocks.join(', '))
     else
         console.log("No polluted areas");
-
 }
-
-
 
 solve([
     "5 7 2 14 4",
@@ -91,9 +68,9 @@ solve([
     "3 16 7 42 12",
     "2 20 8 39 14",
     "7 34 1 10 24",
-  ],
-  ["breeze 1", "gale 2", "smog 35"]
-  );
+],
+    ["breeze 1", "gale 2", "smog 35"]
+);
 
 console.log();
 
@@ -103,14 +80,15 @@ solve([
     "3 16 20 42 12",
     "2 20 10 39 14",
     "7 34 4 27 24",
-  ],
-  [ 
-    "smog 11", "gale 3", 
-    "breeze 1", "smog 2"
-  ]
-  );
+],
+    [
+        "smog 11", "gale 3",
+        "breeze 1", "smog 2"
+    ]
+);
 
-  console.log();
+console.log();
+
 solve([
     "5 7 72 14 4",
     "41 35 37 27 33",
@@ -120,12 +98,3 @@ solve([
 ],
     ["breeze 1", "gale 2", "smog 25"]
 );
-
-
-
-
-
-
-
-
-
