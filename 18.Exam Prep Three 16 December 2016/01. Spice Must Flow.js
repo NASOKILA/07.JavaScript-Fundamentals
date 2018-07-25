4a7297d9cf27675dc92946646ec5b9570d5290f0
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 03:49:01 +0300

    Exam Prep Three 16 December 2016

--- 18.Exam Prep Three 16 December 2016/01. Spice Must Flow.js
+++ 18.Exam Prep Three 16 December 2016/01. Spice Must Flow.js
@@ -1,14 +1,11 @@
 
-
-
 function solve(source) {
 
     let days = 0;
-    let crewConsumption = 26;   //26 per day
+    let crewConsumption = 26;   
     let extractedSpice = 0;
 
-    if(source < 100)
-    {
+    if(source < 100){
         console.log(days);
         console.log(extractedSpice);
         return;
@@ -17,25 +14,17 @@ function solve(source) {
     while (source >= 100) {
 
         days++;
-
         extractedSpice += source;
-        
         extractedSpice -= crewConsumption;
-
         source -= 10;
-        
     }
 
-    //abbandon the source
     extractedSpice -= crewConsumption;
 
     console.log(days);
     console.log(extractedSpice);
-
 }
 
 solve(99);
-
 solve(450);
-
 solve(200);
--- 18.Exam Prep Three 16 December 2016/02. Build a Wall.js
+++ 18.Exam Prep Three 16 December 2016/02. Build a Wall.js
@@ -6,8 +6,6 @@ function solve(initialHeight) {
 
     let totalDailyConcreteUsed = [];
 
-
-    //vurtim cikul dokato vsichki nomera ne stanat 30
     while (initialHeightNumbersParsed.some(n => n < 30)) {
         
         let dailyConcrete = 0;
@@ -18,26 +16,17 @@ function solve(initialHeight) {
                 dailyConcrete += 195;
                 initialHeightNumbersParsed[i]++;
             }
-
         }
+
         totalDailyConcreteUsed.push(dailyConcrete);
     }
-
 
     let totalCostOfTheWall = 0;
     totalDailyConcreteUsed.map(d => totalCostOfTheWall += d);
 
     console.log(totalDailyConcreteUsed.join(', '));
     console.log(totalCostOfTheWall * 1900 + ' pesos')
-
 }
 
 solve([17, 22, 17, 19, 17]);
-
-
 solve(['21', '25', '28']);
-
-
-
-
-
